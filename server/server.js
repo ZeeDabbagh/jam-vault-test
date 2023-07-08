61547cb56cd1457d8aee37a2aea9acd27782e299
Zainab Dabbagh <dabbagh.zainab@gmail.com>
2023-07-08 15:22:39 -0400

    added upload functionality to server, still working 200

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
+const fileUpload = require('express-fileupload');
+// const searchRouter = require('./routes/searchRoute');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -17,9 +19,29 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// app.use('/api', searchRouter)
+app.use(fileUpload());
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.post('/uploads', (req, res) => {
+  if(req.files === null) {
+      return res.status(400).json({message: 'no file was uploaded'})
+  }
+
+  const file =req.files.media
+
+  file.mv(`${__dirname}/../client/build/uploads/${file.name}`, err => {
+      if(err) {
+          console.error(err)
+          return res.status(500).send(err)
+      }
+
+      res.json({ fileName: file.name, filePath: `/uploads/${file.name}`})
+  })
+})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
